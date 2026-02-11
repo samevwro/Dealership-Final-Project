da19ad85242605532d8a52df65f0b95235ba79f0
Sam W <samevwro@gmail.com>
2026-02-10 21:02:32 -0700

    Customer/Employee Post update

--- src/main/java/DealerShip/controller/DealerShipController.java
+++ src/main/java/DealerShip/controller/DealerShipController.java
@@ -2,13 +2,17 @@ package DealerShip.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import DealerShip.controller.model.DealerShipData;
+import DealerShip.controller.model.DealerShipData.DealerShipCustomer;
+import DealerShip.controller.model.DealerShipData.DealerShipEmployee;
 import DealerShip.service.DealerShipService;
 import lombok.extern.slf4j.Slf4j;
 
@@ -23,8 +27,30 @@ public class DealerShipController {
 	@PostMapping("/dealership_post")
 	@ResponseStatus(HttpStatus.CREATED)
 	public DealerShipData insertDealerShip(@RequestBody DealerShipData data) {
-		log.info("Creating DealerShip {}", data);
+		log.info("Creating Dealership {}", data);
 		return dealerShipService.saveDealerShip(data);
+	}
+	
+	@PutMapping("/dealership_put/{dealerShipId}")
+	@ResponseStatus(HttpStatus.OK)
+	public DealerShipData updateDealerShip(@RequestBody DealerShipData data, @PathVariable Long dealerShipId) {
+		data.setDealerShipId(dealerShipId);
+		log.info("Updating Dealership with id={}", dealerShipId);
+		return dealerShipService.saveDealerShip(data);
+	}
+	
+	@PostMapping("/{dealerShipId}/employee")
+	@ResponseStatus(HttpStatus.CREATED)
+	public DealerShipEmployee insertEmployee(@RequestBody DealerShipEmployee data, @PathVariable Long dealerShipId) {
+		log.info("Creating Employee at Dealership={}", dealerShipId);
+		return dealerShipService.saveEmployee(data, dealerShipId);
+	}
+	
+	@PostMapping("/{dealerShipId}/customer")
+	@ResponseStatus(HttpStatus.CREATED)
+	public DealerShipCustomer insertCustomer(@RequestBody DealerShipCustomer data, @PathVariable Long dealerShipId) {
+		log.info("Creating Customer at Dealership={}", dealerShipId);
+		return dealerShipService.saveCustomer(data, dealerShipId);
 	}
 
 
--- src/main/java/DealerShip/service/DealerShipService.java
+++ src/main/java/DealerShip/service/DealerShipService.java
@@ -5,13 +5,18 @@ import java.util.Objects;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import DealerShip.controller.model.DealerShipData;
+import DealerShip.controller.model.DealerShipData.DealerShipCustomer;
+import DealerShip.controller.model.DealerShipData.DealerShipEmployee;
 import DealerShip.dao.CustomerDao;
 import DealerShip.dao.DealerShipDao;
 import DealerShip.dao.EmployeeDao;
 import DealerShip.dao.VehicleDao;
+import DealerShip.entity.Customer;
 import DealerShip.entity.DealerShip;
+import DealerShip.entity.Employee;
 
 @Service
 public class DealerShipService {
@@ -28,6 +33,7 @@ public class DealerShipService {
 	@Autowired
 	private VehicleDao vehicleDao;
 
+	@Transactional(readOnly = false)
 	public DealerShipData saveDealerShip(DealerShipData data) {
 		Long DealerShipId = data.getDealerShipId();
 		DealerShip dealerShip = findOrCreateDealerShip(DealerShipId);
@@ -57,6 +63,80 @@ public class DealerShipService {
 	private DealerShip findDealerShipById(Long dealerShipId) {
 		return dealerShipDao.findById(dealerShipId).orElseThrow(
 				() -> new NoSuchElementException("Dealership with ID=" + dealerShipId + " not found."));
+	}
+
+	public DealerShipEmployee saveEmployee(DealerShipEmployee data, Long dealerShipId) {
+		DealerShip dealerShip = findDealerShipById(dealerShipId);
+		Long employeeId = data.getEmployeeId();
+		Employee employee = findOrCreateEmployee(employeeId, dealerShipId);
+		
+		employee.setDealerShip(dealerShip);
+		copyEmployeeFields(employee, data);
+		
+		dealerShip.getEmployees().add(employee);
+		
+		Employee savedEmployee = employeeDao.save(employee);
+		return new DealerShipEmployee(savedEmployee);
+	}
+
+	private void copyEmployeeFields(Employee employee, DealerShipEmployee data) {
+		employee.setEmployeeFirstName(data.getEmployeeFirstName());
+		employee.setEmployeeLastName(data.getEmployeeLastName());
+		employee.setEmployeePhone(data.getEmployeePhone());
+		employee.setEmployeeEmail(data.getEmployeeEmail());
+		employee.setEmployeeJobTittle(data.getEmployeeJobTittle());
+	}
+
+	private Employee findOrCreateEmployee(Long employeeId, Long dealerShipId) {
+		if(Objects.isNull(dealerShipId)) {
+			new NoSuchElementException("Dealership with ID=" + dealerShipId + " not found");
+			return null;
+		}else {
+		if (Objects.isNull(employeeId)) {
+			return new Employee();
+		} else {
+			return findEmployeeById(employeeId);
+		}}
+	}
+
+	private Employee findEmployeeById(Long employeeId) {
+		return employeeDao.findById(employeeId).orElseThrow(
+				() -> new NoSuchElementException("Employee with ID=" + employeeId + " not found."));
+	}
+
+	public DealerShipCustomer saveCustomer(DealerShipCustomer data, Long dealerShipId) {
+		DealerShip dealerShip = findDealerShipById(dealerShipId);
+		Long customerId = data.getCustomerId();
+		Customer customer = findOrCreateCustomer(customerId, dealerShipId);
+		
+		
+		copyCustomerFields(customer, data);
+		
+		customer.getDealerShip().add(dealerShip);
+		dealerShip.getCustomers().add(customer);
+		
+		Customer savedCustomer = customerDao.save(customer);
+		
+		return new DealerShipCustomer(savedCustomer);
+	}
+
+	private void copyCustomerFields(Customer customer, DealerShipCustomer data) {
+		customer.setCustomerFirstName(data.getCustomerFirstName());
+		customer.setCustomerLastName(data.getCustomerLastName());
+		customer.setCustomerEmail(data.getCustomerEmail());
+	}
+
+	private Customer findOrCreateCustomer(Long customerId, Long dealerShipId) {
+		if(Objects.isNull(customerId)){
+			return new Customer();
+		}else {
+			return findCustomerById(customerId);
+		}
+	}
+
+	private Customer findCustomerById(Long customerId) {
+		return customerDao.findById(customerId).orElseThrow(
+				() -> new NoSuchElementException("Customer with ID=" + customerId + " not found."));
 	}
 	
 }
